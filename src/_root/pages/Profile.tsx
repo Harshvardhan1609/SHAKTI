7b52f1abe8dbf2c5d7f0332a0b4e1d95e9a44768
harshvardhan <harshvardhanpurohit2020@gmail.com>
2024-01-09 20:01:59 +0530

    init

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -62,9 +62,8 @@ import {
               </div>
   
               <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
-                <StatBlock value={currentUser.posts.length} label="Posts" />
-                <StatBlock value={20} label="Followers" />
-                <StatBlock value={20} label="Following" />
+                <StatBlock value={currentUser.posts.length} label="SHAKTI POINTS" />
+                <StatBlock value={"SHAKTI"} label="REPRESENTATIVE" />
               </div>
   
               <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -59,9 +59,12 @@ export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
     queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {
+    getNextPageParam: (lastPage ) => {
+      if (lastPage === undefined){
+        return undefined;
+      }
       if(lastPage && lastPage.documents.length === 0) return null;
-      const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+        const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
       return lastId;
     }
   })
